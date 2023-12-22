e79276b02d1d2f25bc22272f0fa89dac51cd834d
ABEL-pixel-nube <labelardo.477@gmail.com>
2023-12-22 15:35:08 -0500

    Initial commit

---
+++ src/main/java/Resultados/Resultados/ModuloResultadosApplication.java
@@ -0,0 +1,13 @@
+package Resultados.Resultados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloResultadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloResultadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Resultados/Resultados/ModuloResultadosApplicationTests.java
@@ -0,0 +1,13 @@
+package Resultados.Resultados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloResultadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
